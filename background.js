a2460214f65e3c6434b579b521a9461571cf1db6
Min Gi Kwon <64427415+MinGi-K@users.noreply.github.com>
2023-11-25 16:42:56 -0500

    Update background.js to block

--- background.js
+++ background.js
@@ -1,6 +1,9 @@
-chrome.webNavigation.onBeforeNavigate.addListener(function (details) {
-    if (details.url.includes("netflix.com")) {
-      chrome.tabs.update(details.tabId, { url: "https://leetcode.com" });
+chrome.webRequest.onBeforeRequest.addListener(
+  function(details) {
+    if (shouldRedirect()) { // Assuming this is your function to check if blocking is active
+      return {redirectUrl: "https://leetcode.com"}; // Redirect to LeetCode
     }
-  });
-  
+  },
+  {urls: ["*://*.netflix.com/*"]},
+  ["blocking"]
+);
